d5d19eb160df0738bb228ad07af43d01192d388d
Kamil Lelonek <squixy.sln@gmail.com>
2013-09-19 18:00:34 +0200

    Network exception handling
    Fixed craching application when user connects to network that requires
    logging in.

--- PWrRssReader/src/pwr/rss/reader/json/PWrJSONParser.scala
+++ PWrRssReader/src/pwr/rss/reader/json/PWrJSONParser.scala
@@ -3,12 +3,10 @@ package pwr.rss.reader.json
 import com.google.gson.Gson
 
 object PWrJSONParser {
-	def getFeeds(inputString: String) = {
-		val gson = new Gson
-		val feedsCollection = gson.fromJson[FeedsCollection](inputString, classOf[FeedsCollection])
-
-		if (feedsCollection != null)
-			feedsCollection.feeds
-		else null
-	}
+  def getFeeds(inputString: String) =
+    try {
+      val feedsCollection = (new Gson).fromJson[FeedsCollection](inputString, classOf[FeedsCollection])
+      feedsCollection.feeds
+    }
+  	catch { case e: Exception => null }
 }
